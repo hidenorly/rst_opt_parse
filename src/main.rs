fb64b585adb2fedd03b625241f89ceb0381ff306
hidenorly <twitte.harold@gmail.com>
2022-04-18 00:20:32 +0900

    Add sample of main.rs

---
+++ src/main.rs
@@ -0,0 +1,34 @@
+/*
+  Copyright (C) 2022 hidenorly
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+use std::env;
+use rst_opt_parse::IOptParse;
+use rst_opt_parse::OptParse;
+use rst_opt_parse::OptParseItem;
+
+fn main() {
+    let mut options = Vec::new();
+    options.push( OptParseItem::new( "-s", "--samplingRate", true, "48000", "Set Sampling Rate") );
+    options.push( OptParseItem::new( "-e", "--encoding", true, "PCM16", "Set Encoding PCM8, PCM16, PCM24, PCM32, PCMFLOAT") );
+    options.push( OptParseItem::new( "-c", "--channel", true, "2", "Set channel 2, 2.1, 4, 4.1, 5, 5.1, 5.1.2, 7.1") );
+
+    let argv: Vec<String> = env::args().collect();
+
+    let mut opt_parse = OptParse::new( argv, options, "rst_opt_parse_test" );
+    opt_parse.parse_options( false );
+
+    println!( "encoding:{}, samplingRate:{}, channel:{}", opt_parse.get_value("-e"), opt_parse.get_value("-s"), opt_parse.get_value("-c") );
+}
